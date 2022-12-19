3814ac55bd62580a17b8940c43dcf6863b6b337b
Jayasree1802 <121030796+Jayasree1802@users.noreply.github.com>
2022-12-19 23:23:50 +0530

    Add files via upload

---
+++ client.c
@@ -0,0 +1,14 @@
+//client file of auction system
+
+#include"header.h"
+
+
+int main()
+{
+
+
+    selectMenu();                                   //function to select menu
+
+    return 0;
+}
+~              
